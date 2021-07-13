1f859ba91bece313d1637f566ee6f2a6958e4292
RIFAT ABRAR JOWAD <rajdiprifat2004@gmail.com>
2021-07-13 23:04:07 +0600

    variable

---
+++ 01/02.php
@@ -0,0 +1,13 @@
+<?php
+$_cName01 = 'Bangladesh';
+$age = 17 ;
+$old = "age";
+
+echo $$old;
+
+echo "\n";
+$que = "vai tmr boyos koto..??";
+
+echo "hello ".$_cName01;
+echo "\n";
+echo "{$que} \n vai amar boyos {$age} bochor";
